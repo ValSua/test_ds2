52584f4f30824f0e2c3db839576a66dfec39a556
ValSua <109840298+ValSua@users.noreply.github.com>
2024-02-19 20:30:50 -0500

    ejemplo py

---
+++ manage.py
@@ -0,0 +1,19 @@
+def suma(a, b):
+    """
+    Funci¢n que suma dos n£meros.
+
+    Args:
+        a (int): El primer n£mero a sumar.
+        b (int): El segundo n£mero a sumar.
+
+    Returns:
+        int: La suma de los dos n£meros.
+    """
+    resultado = a + b
+    return resultado
+
+# Ejemplo de uso
+resultado_suma = suma(3, 5)
+print("El resultado de la suma es:", resultado_suma)
+
+Ejemplo de funci¢n en python
